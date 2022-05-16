ee26525ea2a050208151c67315b02e6684d83c80
Hammami <ahmed.hammami.1@esprit.tn>
2022-05-16 10:11:39 +0200

    ui fix

--- force-app/main/default/lwc/navBarServicom/navBarServicom.js
+++ force-app/main/default/lwc/navBarServicom/navBarServicom.js
@@ -1,12 +1,64 @@
 import { LightningElement } from 'lwc';
 import Servicom from '@salesforce/resourceUrl/ServicomLogo';
+import { NavigationMixin } from 'lightning/navigation';
 
 
 
-export default class NavBarServicom extends LightningElement {
+export default class NavBarServicom extends NavigationMixin(LightningElement) {
 
     navbarItems =["Home","About Us","Profil",];
     logo=Servicom;
+
+
+    GoToHome() {
+        this[NavigationMixin.Navigate]({
+          type: "standard__webPage",
+          attributes: {
+              url: `/` 
+              
+          }
+      },
+      true
+      );
+      }
+
+      GoToProfile() {
+        this[NavigationMixin.Navigate]({
+          type: "standard__webPage",
+          attributes: {
+              url: `/myservices/` 
+              
+          }
+      },
+      true
+      );
+      }
+
+      GoToAboutUs() {
+        this[NavigationMixin.Navigate]({
+          type: "standard__webPage",
+          attributes: {
+              url: `/myservices/` 
+              
+          }
+      },
+      true
+      );
+      }
+
+      GoToMyServices() {
+        this[NavigationMixin.Navigate]({
+          type: "standard__webPage",
+          attributes: {
+              url: `/myservices/` 
+              
+          }
+      },
+      true
+      );
+      }
+
+
 
 
 }
